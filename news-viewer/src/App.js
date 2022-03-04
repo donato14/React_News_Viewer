d156f562e01b02322ce2df4563b9c68fb1cf6f6e
Kang Kujun <donato14@naver.com>
2022-03-04 22:22:38 +0900

    🚧 Add a data interlocking code

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import NewsList from './components/NewsList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <NewsList />;
+};
 
 export default App;
+
+// import { useState } from 'react';
+// import axios from 'axios';
+
+// const App = () => {
+//   const [data, setData] = useState(null);
+//   const onClick = async () => {
+//     try {
+//       const response = await axios.get(
+//         'https://newsapi.org/v2/top-headlines?country=kr&apiKey=5a4137b7d42e4aef821bbddf57316f28',
+//       );
+//       setData(response.data);
+//     } catch (e) {
+//       console.log(e);
+//     }
+//     // axios
+//     // .get('https://jsonplaceholder.typicode.com/todos/1')
+//     // .then((response) => {
+//     //   setData(response.data);
+//     // });
+//   };
+//   return (
+//     <div>
+//       <div>
+//         <button onClick={onClick}>불러오기</button>
+//       </div>
+//       {data && (
+//         <textarea
+//           rows={7}
+//           value={JSON.stringify(data, null, 2)}
+//           readOnly={true}
+//         />
+//       )}
+//     </div>
+//   );
+// };
+
+// export default App;
