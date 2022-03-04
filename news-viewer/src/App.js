68d5d0dd6881e132d65c07a9f6dea6f252ee1bc3
Kang Kujun <donato14@naver.com>
2022-03-05 00:45:57 +0900

    ♻️ Add React-Router-Dom

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,55 +1,64 @@
-import { useState, useCallback } from 'react';
-import NewsList from './components/NewsList';
-import Categories from './components/Categories';
+import { Route } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
 const App = () => {
-  const [category, setCategory] = useState('all');
-  const onSelect = useCallback((category) => setCategory(category), []);
-
-  return (
-    <>
-      <Categories category={category} onSelect={onSelect} />
-      <NewsList category={category} />
-    </>
-  );
+  return <Route path="/:category?" component={NewsPage} />;
 };
 
 export default App;
 
-// import { useState } from 'react';
-// import axios from 'axios';
+// import { useState, useCallback } from 'react';
+// import NewsList from './components/NewsList';
+// import Categories from './components/Categories';
 
 // const App = () => {
-//   const [data, setData] = useState(null);
-//   const onClick = async () => {
-//     try {
-//       const response = await axios.get(
-//         'https://newsapi.org/v2/top-headlines?country=kr&apiKey=5a4137b7d42e4aef821bbddf57316f28',
-//       );
-//       setData(response.data);
-//     } catch (e) {
-//       console.log(e);
-//     }
-//     // axios
-//     // .get('https://jsonplaceholder.typicode.com/todos/1')
-//     // .then((response) => {
-//     //   setData(response.data);
-//     // });
-//   };
+//   const [category, setCategory] = useState('all');
+//   const onSelect = useCallback((category) => setCategory(category), []);
+
 //   return (
-//     <div>
-//       <div>
-//         <button onClick={onClick}>불러오기</button>
-//       </div>
-//       {data && (
-//         <textarea
-//           rows={7}
-//           value={JSON.stringify(data, null, 2)}
-//           readOnly={true}
-//         />
-//       )}
-//     </div>
+//     <>
+//       <Categories category={category} onSelect={onSelect} />
+//       <NewsList category={category} />
+//     </>
 //   );
 // };
 
 // export default App;
+
+// // import { useState } from 'react';
+// // import axios from 'axios';
+
+// // const App = () => {
+// //   const [data, setData] = useState(null);
+// //   const onClick = async () => {
+// //     try {
+// //       const response = await axios.get(
+// //         'https://newsapi.org/v2/top-headlines?country=kr&apiKey=5a4137b7d42e4aef821bbddf57316f28',
+// //       );
+// //       setData(response.data);
+// //     } catch (e) {
+// //       console.log(e);
+// //     }
+// //     // axios
+// //     // .get('https://jsonplaceholder.typicode.com/todos/1')
+// //     // .then((response) => {
+// //     //   setData(response.data);
+// //     // });
+// //   };
+// //   return (
+// //     <div>
+// //       <div>
+// //         <button onClick={onClick}>불러오기</button>
+// //       </div>
+// //       {data && (
+// //         <textarea
+// //           rows={7}
+// //           value={JSON.stringify(data, null, 2)}
+// //           readOnly={true}
+// //         />
+// //       )}
+// //     </div>
+// //   );
+// // };
+
+// // export default App;
